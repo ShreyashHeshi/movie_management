6dde1f2ec0e3fa45b1d6d61c682c7ec441852bc0
ShreyashHeshi <shreyash.heshi@gmail.com>
2025-05-23 19:01:23 +0530

    redis cache implemented for Get MoviesAPI

--- Movie.API/Movie.API/Controllers/AuthController.cs
+++ Movie.API/Movie.API/Controllers/AuthController.cs
@@ -5,8 +5,8 @@ using Movie.API.Services;
 
 namespace Movie.API.Controllers
 {
-    //[Route("api/[controller]")]
-    //[ApiController]
+    [Route("api/[controller]")]
+    [ApiController]
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
